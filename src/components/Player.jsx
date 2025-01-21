b220afe9528c7a63710edf98e16a32c1a20e301d
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2025-01-21 11:25:42 -0300

    dev branch and Player component

---
+++ src/components/Player.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Player = ({name, symbol}) => {
+   return <li>
+      <span className="player">
+         <span className="player-name">{name}</span>
+         <span className="player-symbol">{symbol}</span>
+      </span>
+      <button type="button">Edit</button>
+   </li>
+}
+
+export default Player;
